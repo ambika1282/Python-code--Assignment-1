da2350afc6b9c1375db21258b825627ed89cf6cf
ambika1282 <83117618+ambika1282@users.noreply.github.com>
2021-05-12 19:47:29 +0530

    Add files via upload

---
+++ Assignment -1 (2).py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+for i in range(2000,3001):
+        if i%7==0 and i%5!=0:
+            print(i,"," ,end=" ")
+            
+
+
+# In[30]:
+
+
+A=input('first name ')
+B=input('last name ')
+print(B,A)
+
+
+# In[31]:
+
+
+import math
+math.pi
+
+
+# In[32]:
+
+
+r=float(input('radius'))
+pi=math.pi
+V=4.0/3.0*pi*r**3
+print(V)
+
+
+# In[ ]:
+
+
+
+
